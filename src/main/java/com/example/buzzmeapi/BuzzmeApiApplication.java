d940d5b02dd9e2df443c24dad7549db935c8061c
deepak.asharma <deepak.asharma@springernature.com>
2019-05-10 12:07:53 +0530

    Added project setup

---
+++ src/main/java/com/example/buzzmeapi/BuzzmeApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.buzzmeapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuzzmeApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuzzmeApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/buzzmeapi/BuzzmeApiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.buzzmeapi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BuzzmeApiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
