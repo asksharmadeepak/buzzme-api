c9a85551791aa53c79fc75a74a1765df8fb1a19a
Tausif <tausif.ksayyad@springernature.com>
2019-05-10 17:07:53 +0530

    Pratik/Tausif Added Repository for CRUD operation

--- src/main/java/com/example/buzzmeapi/models/Buzz.java
+++ src/main/java/com/example/buzzmeapi/models/Buzz.java
@@ -1,15 +1,68 @@
 package com.example.buzzmeapi.models;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import java.util.Date;
 
 @Entity
 public class Buzz {
-    @Id
+    @Id @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
     private Date event;
     private int frequency;
     private String modeOfNotification;
     private String email;
+
+    public Buzz() {
+    }
+
+    public Buzz(long id, Date event, int frequency, String modeOfNotification, String email) {
+        this.id = id;
+        this.event = event;
+        this.frequency = frequency;
+        this.modeOfNotification = modeOfNotification;
+        this.email = email;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public Date getEvent() {
+        return event;
+    }
+
+    public void setEvent(Date event) {
+        this.event = event;
+    }
+
+    public int getFrequency() {
+        return frequency;
+    }
+
+    public void setFrequency(int frequency) {
+        this.frequency = frequency;
+    }
+
+    public String getModeOfNotification() {
+        return modeOfNotification;
+    }
+
+    public void setModeOfNotification(String modeOfNotification) {
+        this.modeOfNotification = modeOfNotification;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
---
+++ src/main/java/com/example/buzzmeapi/repositories/BuzzRepository.java
@@ -0,0 +1,19 @@
+package com.example.buzzmeapi.repositories;
+
+import com.example.buzzmeapi.models.Buzz;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class BuzzRepository {
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+
+    public int buzz(Buzz buzz) {
+        return jdbcTemplate.update("insert into buzz(event, frequency, modeOfNotification, email)" + " values(?, ?, ? ,?)",
+                new Object[]{
+                        buzz.getEvent(), buzz.getFrequency(), buzz.getModeOfNotification(), buzz.getEmail()
+                });
+    }
+}
