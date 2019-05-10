b0e9b14de1d749611915bd7fc520d0abc6e38c72
deepak.asharma <deepak.asharma@springernature.com>
2019-05-10 12:56:49 +0530

    Added send mail endpoint configs

--- src/main/java/com/example/buzzmeapi/controllers/SendMailController.java
+++ src/main/java/com/example/buzzmeapi/controllers/SendMailController.java
@@ -9,6 +9,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.mail.MessagingException;
+
 @Controller
 public class SendMailController {
 
@@ -18,8 +20,8 @@ public class SendMailController {
     @RequestMapping(EndPoints.SEND_MAIL)
     @ResponseBody
     @CrossOrigin(origins = "http://localhost:4200")
-    public void getAirportDetails(@RequestParam(required = false) String mailId,
-                                  @RequestParam(required = false) String payload) {
+    public void sendMailTo(@RequestParam(required = false) String mailId,
+                                  @RequestParam(required = false) String payload) throws MessagingException {
         mailServiceImpl.sendNotificationOnMail(mailId, payload);
     }
 }
--- src/main/java/com/example/buzzmeapi/services/MailServiceImpl.java
+++ src/main/java/com/example/buzzmeapi/services/MailServiceImpl.java
@@ -2,8 +2,15 @@ package com.example.buzzmeapi.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.MailSender;
-import org.springframework.mail.SimpleMailMessage;
 import org.springframework.stereotype.Component;
+
+import javax.mail.*;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
+import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
+import java.util.Date;
+import java.util.Properties;
 
 @Component
 public class MailServiceImpl {
@@ -12,15 +19,35 @@ public class MailServiceImpl {
     @Autowired
     private MailSender mailSender;
 
-    public void sendNotificationOnMail(String mailId, String payload) {
+    public void sendNotificationOnMail(String mailId, String payload) throws MessagingException {
 
-        SimpleMailMessage message = new SimpleMailMessage();
+        Properties props = new Properties();
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.smtp.host", "smtp.gmail.com");
+        props.put("mail.smtp.port", "587");
 
-        message.setFrom("deepsharma989@gmail.com");
-        message.setTo("milandalsaniya@yahoo.com"); //mailId
-        message.setSubject("Hi Buzz Me notification arrived !!!");
-        message.setText("Please book you ticket on this date"); // Payload
-        mailSender.send(message);
+        Session session = Session.getInstance(props, new javax.mail.Authenticator() {
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication("deepsharma989@gmail.com", "deepak2015");
+            }
+        });
+        Message msg = new MimeMessage(session);
+        msg.setFrom(new InternetAddress("deepsharma989@gmail.com", false));
+
+        msg.setRecipients(Message.RecipientType.TO, InternetAddress.parse("deepsharma989@gmail.com"));
+        msg.setSubject("Hi Buzz Me notification arrived !!!");
+        msg.setContent("Please book you ticket on this date", "text/html");
+        msg.setSentDate(new Date());
+
+        MimeBodyPart messageBodyPart = new MimeBodyPart();
+        messageBodyPart.setContent("Please book you ticket on this date", "text/html");
+
+        Multipart multipart = new MimeMultipart();
+        multipart.addBodyPart(messageBodyPart);
+        msg.setContent(multipart);
+        Transport.send(msg);
+
 
     }
 }
